b374af62c641934d26e52458cada1cd6a6c65be8
Mukesh Yadav <mukesh.yadav@mindtickle.com>
2022-11-07 15:22:37 +0530

    package added

--- Package.swift
+++ Package.swift
@@ -27,7 +27,9 @@ import PackageDescription
 let package = Package(name: "AlamofireNetworkActivityIndicator",
                       platforms: [.iOS(.v10)],
                       products: [.library(name: "AlamofireNetworkActivityIndicator", targets: ["AlamofireNetworkActivityIndicator"])],
-                      dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", exact: "4.9.0")],
+                      dependencies: [
+                        .package(url: "https://github.com/Alamofire/Alamofire.git", .exactItem("4.9.0"))
+                      ],
                       targets: [.target(name: "AlamofireNetworkActivityIndicator",
                                         dependencies: ["Alamofire"],
                                         path: "Source")])
